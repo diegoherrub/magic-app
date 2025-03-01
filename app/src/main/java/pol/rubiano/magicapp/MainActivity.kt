8c4306f89c35fa29ddc777aae4bef6ad8f44aa1c
Diego Hernández Rubiano <diego.herrub@educa.jcyl.es>
2025-03-01 12:39:25 +0100

    impemented navigation bar with safe args

--- app/src/main/java/pol/rubiano/magicapp/MainActivity.kt
+++ app/src/main/java/pol/rubiano/magicapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package pol.rubiano.magicapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupView()
+    }
+
+    private fun setupView(){
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            Log.d("@POL", "Destino: ${destination.label}")
+        }
     }
 }
